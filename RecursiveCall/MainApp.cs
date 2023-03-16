e6ed17f687915fda9312b645274cbf04064bc6ae
강동윤 <davidkang8111@gmail.com>
2023-03-16 18:55:57 +0900

    초기 커밋.

---
+++ RecursiveCall/MainApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace RecursiveCall
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            MainApp a = new MainApp();
+            int result = a.Factorial(5);
+            Console.WriteLine(result);
+        }
+
+        int Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
+        }
+    }
+}
---
+++ SwapByRef/MainApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SwapByRef
+{
+    class MainApp
+    {
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
