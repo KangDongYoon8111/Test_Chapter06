ed7ead6a526e1b3bd06fa629cd05c2346713ff39
동윤 강 <kdystory@naver.com>
2023-03-26 15:19:13 +0900

    202303261519

---
+++ Ex6_1/MainApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex6_1
+{
+    class MainApp
+    {
+        static double Square(double arg)
+        {
+            return arg * arg;
+            //return Math.Pow(arg, 2);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("수를 입력하세요 : ");
+            string input = Console.ReadLine();
+            double arg = Convert.ToDouble(input);
+
+            Console.WriteLine("결과 : {0}", Square(arg));
+        }
+    }
+}
---
+++ Ex6_2/MainApp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ex6_2
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            double mean = 0;
+
+            // 아래 형태로 보내는 인수의 형태는 기본형(값(복사)형식)
+            // Mean(1, 2, 3, 4, 5, mean);
+            // 변경 1
+            // Mean(1, 2, 3, 4, 5,ref mean);
+            // 변경 2
+            Mean(1, 2, 3, 4, 5, out mean);
+
+            Console.WriteLine("평균 : {0}", mean);
+        }
+
+        public static void Mean(double a, double b, double c,
+            double d, double e, double mean)
+        {
+            mean = (a + b + c + d + e) / 5;
+        }
+
+        //public static void Mean(double a, double b, double c,
+        //    double d, double e,ref double mean)
+        //{
+        //    mean = (a + b + c + d + e) / 5;
+        //}
+
+        public static void Mean(double a, double b, double c,
+            double d, double e, out double mean)
+        {
+            mean = (a + b + c + d + e) / 5;
+        }
+    }
+}
